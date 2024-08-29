351f8e3b8d6434689b9be3a64dd4aca2959720d0
Victor Lopez <victorandreslopez314@gmail.com>
2024-08-29 01:11:31 -0500

    Style left column
    I got the css right for having my links
    in the center and my social media links
    at the footer, while keeping my name and
    title at the top. Will have to find a way
    to keep my name and title in one line as
    the screen width shrinks. Will probably
    have to figure out media queries.

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,26 +3,30 @@ import './Sidebar.css';
 
 function Sidebar() {
     return(
-        <div>
+        <div className='sidebar'>
 
-            <div className='side-header'>
+            <header className='header'>
                 <h1>
                     <a href='/' className='name'>Victor Lopez</a>
                 </h1>
-                <h2 className='title'>Software engineer</h2>
+                <h2 className='title'>Frontend Developer</h2>
+            </header>
+
+            <div className='center-links'>
+                <ul>
+                    <li><a href='victor-lopez.dev' className='about'>About</a></li>
+                    <li><a href='victor-lopez.dev' className='experience'>Experience</a></li>
+                    <li><a href='victor-lopez.dev' className='projects'>Projects</a></li>
+                </ul>
             </div>
 
-            <ul>
-                <li><a href='victor-lopez.dev' className='about'><span>About</span></a></li>
-                <li><a href='victor-lopez.dev' className='experience'><span>Experience</span></a></li>
-                <li><a href='victor-lopez.dev' className='projects'><span>Projects</span></a></li>
-            </ul>
-
-            <ul>
-                <li><a href='https://github.com/victora-lopez/' className='github'><span className='label'>Github</span></a></li>
-                <li><a href='https://www.linkedin.com/in/victora-lopez/' className='linkedin'><span className='label'>Linkedin</span></a></li>
-                <li><a href='https://medium.com/@victorandreslopez314' className='medium'><span className='label'>Medium</span></a></li>
-            </ul>
+            <footer className='footer-links'>
+                <ul className='logos'>
+                    <li><a href='https://github.com/victora-lopez/' className='github'><span className='label'>Github</span></a></li>
+                    <li><a href='https://www.linkedin.com/in/victora-lopez/' className='linkedin'><span className='label'>Linkedin</span></a></li>
+                    <li><a href='https://medium.com/@victorandreslopez314' className='medium'><span className='label'>Medium</span></a></li>
+                </ul>
+            </footer>
         </div>
     )
 }
