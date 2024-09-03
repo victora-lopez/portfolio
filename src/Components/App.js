6af58187ac983430441a2028815e0e5353f56b3a
Victor Lopez <victorandreslopez314@gmail.com>
2024-09-03 16:21:25 -0500

    Added tailwind CSS and restyling header
    Tailwind CSS will help me with styling
    my pages more efficiently and cleanly
    without having to make my own css files
    unless there is something specific i need.
    I am current trying to center the nav bar
    and get the spacing right between the list
    indices.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
-import './App.css';
 import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar></Sidebar>
+    <div className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0'>
+      <div className='lg:flex lg:justify-between lg:gap-4'>
+        <Sidebar />
+      </div>
     </div>
   );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,33 +1,31 @@
 import React from 'react';
-import './Sidebar.css';
 
 function Sidebar() {
     return(
-        <div className='sidebar'>
+                <header className='lg:sticky lg:top-0 lg:flex lg:min-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24'>
+                        <div>
+                            <h1>
+                                <a href='/' className='text-4xl font-bold tracking-tight sm:text-5xl'>Victor Lopez</a>
+                            </h1>
+                            <h2 className='mt-3 text-lg font-medium tracking-tight sm:text-xl'>Frontend Developer</h2>
+                        
 
-            <header className='header'>
-                <h1>
-                    <a href='/' className='name'>Victor Lopez</a>
-                </h1>
-                <h2 className='title'>Frontend Developer</h2>
-            </header>
-
-            <div className='center-links'>
-                <ul>
-                    <li><a href='victor-lopez.dev' className='about'>About</a></li>
-                    <li><a href='victor-lopez.dev' className='experience'>Experience</a></li>
-                    <li><a href='victor-lopez.dev' className='projects'>Projects</a></li>
-                </ul>
-            </div>
-
-            <footer className='footer-links'>
-                <ul className='logos'>
-                    <li><a href='https://github.com/victora-lopez/' className='github'><span className='label'>Github</span></a></li>
-                    <li><a href='https://www.linkedin.com/in/victora-lopez/' className='linkedin'><span className='label'>Linkedin</span></a></li>
-                    <li><a href='https://medium.com/@victorandreslopez314' className='medium'><span className='label'>Medium</span></a></li>
-                </ul>
-            </footer>
-        </div>
+                            <div className=''>
+                                <nav className='hidden lg:block'>
+                                    <ul>
+                                        <li><a href='#about' className='about'>About</a></li>
+                                        <li><a href='#experience' className='experience'>Experience</a></li>
+                                        <li><a href='#projects' className='projects'>Projects</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <ul className='flex '>
+                            <li><a href='https://github.com/victora-lopez/' className='github'><span className='label'>Github</span></a></li>
+                            <li><a href='https://www.linkedin.com/in/victora-lopez/' className='linkedin'><span className='label'>Linkedin</span></a></li>
+                            <li><a href='https://medium.com/@victorandreslopez314' className='medium'><span className='label'>Medium</span></a></li>
+                        </ul>
+                </header>
     )
 }
 
