688719da4de887ee488991ef43a359b91edb5ccb
Victor Lopez <victorandreslopez314@gmail.com>
2024-09-04 20:44:26 -0500

    Add AwesomeFonts social media icons
    Added the package to the project and
    then read the documentation on how to
    add the icons using their component.
    They still need to be styled to be a
    bit bigger but the icons are working.

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,3 +1,5 @@
+import { faGithub, faLinkedinIn, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 function Sidebar() {
@@ -8,23 +10,23 @@ function Sidebar() {
                                 <a href='/' className='text-4xl font-bold tracking-tight sm:text-5xl'>Victor Lopez</a>
                             </h1>
                             <h2 className='mt-3 text-lg font-medium tracking-tight sm:text-xl'>Frontend Developer</h2>
-                        
-
-                            <div className=''>
-                                <nav className='hidden lg:block'>
-                                    <ul>
-                                        <li><a href='#about' className='about'>About</a></li>
-                                        <li><a href='#experience' className='experience'>Experience</a></li>
-                                        <li><a href='#projects' className='projects'>Projects</a></li>
-                                    </ul>
-                                </nav>
-                            </div>
                         </div>
-                        <ul className='flex '>
-                            <li><a href='https://github.com/victora-lopez/' className='github'><span className='label'>Github</span></a></li>
-                            <li><a href='https://www.linkedin.com/in/victora-lopez/' className='linkedin'><span className='label'>Linkedin</span></a></li>
-                            <li><a href='https://medium.com/@victorandreslopez314' className='medium'><span className='label'>Medium</span></a></li>
-                        </ul>
+
+                        <div>
+                            <nav className='hidden lg:block'>
+                                <ul>
+                                    <li><a href='#about' className='about'>About</a></li>
+                                    <li><a href='#experience' className='experience'>Experience</a></li>
+                                    <li><a href='#projects' className='projects'>Projects</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        
+                        <div className='flex gap-4 tailwindfontsizebiggerforicons'>
+                            <a href='https://www.linkedin.com/in/victora-lopez/'><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                            <a href='https://github.com/victora-lopez/'><FontAwesomeIcon icon={faGithub} /></a>
+                            <a href='https://medium.com/@victorandreslopez314'><FontAwesomeIcon icon={faMedium} /></a>
+                        </div>
                 </header>
     )
 }
