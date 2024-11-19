fc0e9fc567b3491b417b379436ecc268854f5600
Victor Lopez <victorandreslopez314@gmail.com>
2024-11-19 01:55:00 -0600

    Fixed styling of work experience titles
    Changed the cursor to pointer when hovering over the job
    added the description for JPMC job, and almost have the
    logic completed for showing the right description when
    the right job is selected.

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -7,11 +7,7 @@ function Experience() {
         {value: "JPMC", label: "Software Engineer"},
         {value: "TI", label: "Product Marketing Engineer"},
         {value: "IEEE", label: "Director of Technology"},
-    ]
-
-    const descriptions = [
-        {job: ""}
-    ]
+    ]  
 
     const [selectedJob, setSelectedJob] = useState("JPMC");
 
@@ -24,9 +20,9 @@ function Experience() {
         <section id="Experience">
             <h1 className="font-bold text-lg mb-8 sm:text-2xl">Work Experience</h1>
             <div className="flex flex-col">
-                <form className="flex gap-8 text-sm justify-between border-b border-black job-group">
+                <form className="flex gap-8 text-sm justify-evenly border-b border-black job-group">
                     {jobs.map( (job) => (
-                                <label key={job.value} className={selectedJob === job.value ? "font-bold": ""}>
+                                <label key={job.value} className={selectedJob === job.value ? "selected hover:cursor-pointer": "hover:cursor-pointer"}>
                                     <input
                                     type="radio"
                                     name="jobs"
@@ -41,17 +37,17 @@ function Experience() {
                         )
                     }
                 </form>
-                <ul className="px-4 list-disc">
-                    <li>filler 1</li>
-                    <li>filler 1</li>
-                    <li>filler 1</li>
+                <ul className= {selectedJob === jobs[0].value ? "px-4 text-sm list-disc" :"px-4 text-sm list-disc hidden" }>
+                    <li>Joining JPMorgan's Software Engineer Program to develop scalable and innovative solutions.</li>
+                    <li>Advancing technical and leadership skills through mentorship and training.</li>
+                    <li>Collaborating with global teams to design and deliver high-quality software.</li>
                 </ul>
-                <ul className="px-4 list-disc hidden">
+                <ul className="px-4 text-sm list-disc hidden">
                     <li>filler 2</li>
                     <li>filler 2</li>
                     <li>filler 2</li>
                 </ul>
-                <ul className="px-4 list-disc hidden">
+                <ul className="px-4 text-sm list-disc hidden">
                     <li>filler 3</li>
                     <li>filler 3</li>
                     <li>filler 3</li>
